8bb139d972c5b2ac5be202bdcb3b10e221e9478f
Nicolas Seglenieks <nseglenieks@gmail.com>
2022-11-20 15:28:28 -0500

    Use SDL2 to draw to the screen

--- src/main.c
+++ src/main.c
@@ -1,16 +1,103 @@
 #define SDL_MAIN_HANDLED
 
+#include <stdbool.h>
+
 #include "SDL.h"
 
 #include "chip8.h"
+
+const int SDL_SCREEN_WIDTH  = 1024;
+const int SDL_SCREEN_HEIGHT = 512;
 
 int main(int argc, char** argv)
 {
     chip8 chip8;
     initChip8(&chip8);
+    loadChip8("Space Invaders [David Winter].ch8", &chip8);
 
-    // make it so you can use this through the command line???
-    SDL_Window* window; 
+    // initialize sdl
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL2 failed to initialize!");
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("chip8 emulator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SDL_SCREEN_WIDTH, SDL_SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (window == NULL)
+    {
+        printf("SDL2 window failed to be created!");
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL)
+    {
+        printf("SDL2 renderer failed to be created!");
+        return 1;
+    }
+
+    // clear the screen black
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+    bool running = true;
+
+    while (running)
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
+                running = false;
+        }
+
+        emulateChip8Cycle(&chip8);
+
+        // when an opcode has come in that has indicated we need to update the screen
+        if (chip8.drawFlag)
+        {
+            // clear the screen black
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderClear(renderer);
+
+            // set the render colour to white so that the pixels are drawn white
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+
+            // draw all the pixels
+            SDL_Rect pixelRect;
+            pixelRect.w = SDL_SCREEN_WIDTH / 64;
+            pixelRect.h = SDL_SCREEN_HEIGHT / 32;
+
+            for (int y = 0; y < 32; y++)
+            {
+                for (int x = 0; x < 64; x++)
+                {
+                    // if the pixel is set 
+                    if (chip8.pixels[x + y * 64] == 1)
+                    {
+                        pixelRect.x = x * pixelRect.w;
+                        pixelRect.y = y * pixelRect.h;
+                        SDL_RenderFillRect(renderer, &pixelRect);
+                    }
+                }
+            }
+
+            // reset the draw flag
+            chip8.drawFlag = false;
+
+            // update the sdl screen
+            SDL_RenderPresent(renderer);
+        }
+
+        SDL_Delay(15);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
